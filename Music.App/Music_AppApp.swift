cea6881df125d747f53f93a41d75edab873f5478
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-05-30 14:32:56 -0600

    Initial Commit

---
+++ Music.App/Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_AppApp.swift
+//  Music.App
+//
+//  Created by Benjamin Rojo on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
